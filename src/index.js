420b6a2184036296ce752a6de46fddd0f05be695
Yuliya Laptsinskaya <89513166+Kroshka0001@users.noreply.github.com>
2023-02-25 15:29:07 +0300

    feat^

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports =  function reverse (ints) {
+    let rev = '';
+    ints = Math.abs(ints);
+    ints = arg.toString();
+
+    for (let i = 0; i < ints.length; i++) {
+
+      rev = ints[i] + rev;
+
+    }
+
+    return Number(rev);
+
+  }
